72196442033683dfd76237eb9d3ef4e2b6bbbade
pramodkmsp <pramodkmsp1995@gmail.com>
2022-08-02 19:53:57 +0530

    CORS enabled in server.js

--- server.js
+++ server.js
@@ -2,10 +2,19 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-app.options('*', cors())
+var whitelist = ['https://newsapi.org/']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  if (whitelist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
+  }
+  callback(null, corsOptions) // callback expects two parameters: error and options
+}
 
 app.use(express.static(__dirname + '/dist/top-ten-news'));
-app.get('/*', function(req, res) {
+app.get('/*', cors(corsOptionsDelegate), function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/top-ten-news/index.html'));
 });
 
