43640afaa15a7b4f75317c467740ebb160d81b7b
pramodkmsp <pramodkmsp1995@gmail.com>
2022-08-02 19:13:20 +0530

    CORS enabled in server.js

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+
+const allowedOrigins = ['https://newsapi.org/v2/'];
+app.use(cors({
+    credentials: true,
+    origin: (origin, callback) => {
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true) 
+      } else {
+        callback(new Error(`Origin: ${origin} is now allowed`))
+      }
+    }
+}));
+
 app.use(express.static(__dirname + '/dist/top-ten-news'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/top-ten-news/index.html'));
 });
+
 app.listen(process.env.PORT || 8080);
