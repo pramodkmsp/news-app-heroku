f0039db3243f93ef3b078108e892998df2d46304
pramodkmsp <pramodkmsp1995@gmail.com>
2022-08-02 19:42:01 +0530

    CORS enabled in server.js

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-app.configure(function () {
-    app.use(allowCrossDomain);
-})
+const cors = require('cors');
+app.options('*', cors())
 
 app.use(express.static(__dirname + '/dist/top-ten-news'));
 app.get('/*', function(req, res) {
