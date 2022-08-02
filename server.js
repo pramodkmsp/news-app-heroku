b309d39a4e04f71c911e20c37071a41f4150ddd8
pramodkmsp <pramodkmsp1995@gmail.com>
2022-08-02 19:27:38 +0530

    CORS enabled in server.js

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
-const allowedOrigins = ['https://newsapi.org/v2/'];
+const cors = require('cors');
+app.all('*', function(req, res, next) {
+    const origin = req.get('origin'); 
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 app.use(express.static(__dirname + '/dist/top-ten-news'));
 app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/top-ten-news/index.html'));
 });
-app.use(cors({
-    credentials: true,
-    origin: (origin, callback) => {
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true) 
-      } else {
-        callback(new Error(`Origin: ${origin} is now allowed`))
-      }
-    }
-}));
+
 app.listen(process.env.PORT || 8080);
