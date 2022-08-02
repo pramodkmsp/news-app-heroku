d639bfc898e184c8e04de075daa98ed4f759205f
pramodkmsp <pramodkmsp1995@gmail.com>
2022-08-02 19:29:49 +0530

    CORS enabled in server.js

--- server.js
+++ server.js
@@ -1,14 +1,6 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const cors = require('cors');
-app.all('*', function(req, res, next) {
-    const origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
 
 app.use(express.static(__dirname + '/dist/top-ten-news'));
 app.get('/*', function(req, res) {
